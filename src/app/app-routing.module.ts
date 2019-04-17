071fc3e597eaa950c86b2c3bb1cd57190836dfa2
Ananya Kay <2082542@gmail.com>
2019-04-17 11:32:17 +0700

    kay

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'profile', loadChildren: './profile/profile.module#ProfilePageModule' },
+  { path: 'portfolio', loadChildren: './portfolio/portfolio.module#PortfolioPageModule' }
 
 ];
 @NgModule({
